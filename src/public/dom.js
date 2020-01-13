da3b7d7bca01cd2601876ac21e81f0571756a172
James Abney <james.abney@gmail.com>
2020-01-13 12:54:47 -0800

    Create dom.js

---
+++ src/public/dom.js
@@ -0,0 +1,88 @@
+(() => {
+  /**
+   * @param {string} selector
+   */
+  function findAll(selector) {
+    return document.querySelectorAll(selector)
+  }
+
+  /**
+   * @param {string} selector
+   */
+  function findOne(selector) {
+    return document.querySelector(selector)
+  }
+
+  /**
+   * Create and return a DOM element.
+   *
+   * @param {string} tag
+   * @param {{[key: string]: string}} [attr]
+   * @param {Element[]} [children]
+   */
+  function createElement(tag, attr = {}, children = []) {
+    // Crete the root element.
+    const element = document.createElement(tag)
+
+    // Set attributes.
+    Object.entries(attr).forEach(([key, value]) => element.setAttribute(key, value))
+
+    // Append children.
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        const text = document.createTextNode(child)
+        element.appendChild(text)
+      } else {
+        element.appendChild(child)
+      }
+    })
+
+    return element
+  }
+
+  /**
+   *
+   * @param {string|Element} parent
+   * @param {Element[]} children
+   */
+  function append(parent, children) {
+    const element = typeof parent === 'string' ? findOne(parent) : parent
+
+    if (element == null) {
+      throw new Error(`element not found for selector ${parent}`)
+    }
+
+    children.forEach(child => element.appendChild(child))
+    return element
+  }
+
+  /**
+   * @param {string|Element} parent
+   */
+  function empty(parent) {
+    const element = typeof parent === 'string' ? findOne(parent) : parent
+
+    if (element == null) {
+      throw new Error(`element not found for selector ${parent}`)
+    }
+
+    while(element.children.length > 0) {
+      element.removeChild(element.lastChild)
+    }
+
+    return element
+  }
+
+  /**
+   * @type {import('../../declarations').DomLib}
+   */
+  const domLib = {
+    findAll,
+    findOne,
+    createElement,
+    append,
+    empty,
+  }
+
+  window.dom = domLib
+})()
