782e3675af621b486acb0d43e72b10e0b77794c6
James Abney <james.abney@gmail.com>
2020-01-13 16:33:18 -0800

    Add toJSON method

--- src/models/room.js
+++ src/models/room.js
@@ -13,7 +13,6 @@ class Room {
     this.title = title
     this.namespace = namespace
     this.isPrivate = isPrivate
-
     this.history = []
   }
 
@@ -30,6 +29,19 @@ class Room {
    */
   clearHistory() {
     this.history = []
+  }
+
+  /**
+   * Serialization.
+   */
+  toJSON() {
+    return {
+      id: this.id,
+      title: this.title,
+      isPrivate: this.isPrivate,
+      namespace: this.namespace.title,
+      history: this.history,
+    }
   }
 }
 
