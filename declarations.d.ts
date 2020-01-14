fab80145236c0fba25563d8ab6b57c517fbdfafc
James Abney <james.abney@gmail.com>
2020-01-13 17:06:11 -0800

    Add IoHelpers interface and associated defs

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,17 +3,38 @@ export interface Attributes {
 }
 
 export interface DomLib {
-  findOne: (sel: string) => Element
-  findAll: (sel: string) => NodeListOf<Element>
+  findOne(sel: string): Element
+  findAll(sel: string): NodeListOf<Element>
   createElement(tag: string, attr?: Attributes, children?: (string|Element)[]): Element
   append(parent: string|Element, children: Element[]): Element
   empty(parent: string|Element): Element
 }
 
+export interface NsData {
+  title: string
+  img: string
+  endpoint: string
+}
+
+export interface RoomData {
+  id: number
+  title: string
+  nsTitle: string
+  isPrivate: boolean
+  history: string[]
+}
+
+export interface IoHelpers {
+  nsToElement(cb: (element: Element, ns: NsData) => void, ns: NsData): Element
+  roomToElement(cb: (element: Element, room: RoomData) => void, room: RoomData): Element
+}
+
 declare global {
   const dom: DomLib
+  const ioHelpers: IoHelpers
 
   interface Window {
     dom: DomLib
+    ioHelpers: IoHelpers
   }
 }
