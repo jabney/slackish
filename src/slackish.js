12bb9488128ab102ead89512da4cdd1da25a8f8c
James Abney <james.abney@gmail.com>
2020-01-15 06:39:42 -0800

    Comments

--- src/slackish.js
+++ src/slackish.js
@@ -2,7 +2,9 @@ const socketio = require('socket.io')
 const app = require('./app')
 const initIo = require('./io')
 
+// Create the app and hand it to socketio. This is our io server.
 const server = app()
 const io = socketio(server)
 
+// Initiate io and wait for connections.
 initIo(io)
