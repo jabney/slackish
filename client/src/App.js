b0d27484c4c8784f815dbbed9775e252173b2013
James Abney <james.abney@gmail.com>
2020-01-20 11:19:09 -0800

    Style app frame

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
+
+const App = () => (
+  <Container fluid className='App no-gutter'>
+    <Row className="frame no-gutter">
+      <Col xs={1} className='namespace no-gutter'></Col>
+      <Col xs={3} className='room'></Col>
+      <Col xs={8} className='chat'></Col>
+    </Row>
+  </Container>
+)
 
 export default App;
