ab175a9b6192943db58c747ebd3ad8d19288738e
James Abney <james.abney@gmail.com>
2020-01-21 12:10:56 -0800

    Icon swap

--- client/src/components/room-item/room-item.component.jsx
+++ client/src/components/room-item/room-item.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { ReactComponent as Lock } from 'bootstrap-icons/icons/lock-fill.svg'
-import { ReactComponent as Unlock } from 'bootstrap-icons/icons/unlock-fill.svg'
+import { ReactComponent as Chat } from 'bootstrap-icons/icons/chat-fill.svg'
 import './room-item.component.scss'
 
 const RoomItem = ({ room, selected, onClick }) => (
   <div className={'RoomItem' + (selected ? ' selected' : '')} onClick={() => onClick()}>
-    { room.isPrivate ? <Lock className='icon' /> : <Unlock className='icon' /> }
+    { room.isPrivate ? <Lock className='icon' /> : <Chat className='icon' /> }
     <span className='title'>{room.title}</span>
   </div>
 )
