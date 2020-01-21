6fc151d1e5353055d820271527f3d8cb8dc69b96
James Abney <james.abney@gmail.com>
2020-01-20 20:40:28 -0800

    Dispatch namespaces on receive from socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { updateNamespaces } from './store/actions'
 
 import LoginModal from './components/login-modal/login-modal.component'
 import Namespaces from './components/namespaces/namespaces.component'
@@ -9,23 +11,37 @@ import Chat from './components/chat/chat.component'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss'
 
-const App = () => (
-  <>
-  <LoginModal />
-  <Container fluid className='App no-gutter'>
-    <Row className="frame no-gutter">
-      <Col xs={1} className='namespace no-gutter'>
-        <Namespaces />
-      </Col>
-      <Col xs={3} className='room'>
-        <Rooms />
-      </Col>
-      <Col xs={8} className='chat'>
-        <Chat />
-      </Col>
-    </Row>
-  </Container>
-  </>
-)
+import io from 'socket.io-client'
 
-export default App
+const App = ({ updateNamespaces }) => {
+  const socket = io()
+
+  socket.on('namespaces', (namespaces) => {
+    updateNamespaces(namespaces)
+  })
+
+  return <>
+    <LoginModal />
+    <Container fluid className='App no-gutter'>
+      <Row className="frame no-gutter">
+        <Col xs={1} className='namespace no-gutter'>
+          <Namespaces />
+        </Col>
+        <Col xs={3} className='room'>
+          <Rooms />
+        </Col>
+        <Col xs={8} className='chat'>
+          <Chat />
+        </Col>
+      </Row>
+    </Container>
+  </>
+}
+
+const mapState = (state) => ({})
+
+const mapDispatch = (dispatch) => ({
+  updateNamespaces: (nss) => dispatch(updateNamespaces(nss))
+})
+
+export default connect(mapState, mapDispatch)(App)
