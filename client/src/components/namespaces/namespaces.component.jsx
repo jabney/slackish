fe724cb7b0ff3f5047b2fb77220273022d96b89a
James Abney <james.abney@gmail.com>
2020-01-20 20:39:24 -0800

    Iterate and render namespaces

--- client/src/components/namespaces/namespaces.component.jsx
+++ client/src/components/namespaces/namespaces.component.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import './namespaces.component.scss'
+import NamespaceItem from '../namespace-item/namespace-item.component'
 
-const Namespaces = () => (
-  <div className="Namespaces">Namespaces</div>
+const Namespaces = ({ namespaces }) => (
+  <div className="Namespaces">
+    {
+      namespaces.map(ns => <NamespaceItem key={ns.endpoint} ns={ns} />)
+    }
+  </div>
 )
 
-const mapState = () => ({})
+const mapState = (state) => ({
+  namespaces: state.namespaces
+})
+
 const mapDispatch = () => ({})
 
 export default connect(mapState, mapDispatch)(Namespaces)
