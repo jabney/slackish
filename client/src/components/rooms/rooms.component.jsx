ff24088a99a21e6d0b683c8ab71b2da36b71fc07
James Abney <james.abney@gmail.com>
2020-01-22 13:38:43 -0800

    Refactor rooms state management

--- client/src/components/rooms/rooms.component.jsx
+++ client/src/components/rooms/rooms.component.jsx
@@ -9,33 +9,32 @@ import './rooms.component.scss'
 /**
  * @type {React.FunctionComponent}
  */
-const Rooms = ({ namespace, user, selectRoom }) => {
-  const selected = (room) => room.title === namespace.currentRoom
+const Rooms = ({ nsTitle, currentRoom, rooms, user, selectRoom }) => {
+  const selected = (room) => room.title === currentRoom
 
   /**
    * Select a start rooom if a current room is not selected.
    */
   const selectStartRoom = () => {
     // Return early if the current room is set.
-    if (namespace.currentRoom != null) { return }
+    if (currentRoom != null) { return }
 
-    const { rooms } = namespace
     // If there are rooms, select one.
-    if (Array.isArray(rooms) && rooms.length > 0) {
+    if (rooms.length > 0) {
       // Only select a room if the user has been set.
       if (user != null) {
-        selectRoom(namespace.rooms[0])
+        selectRoom(rooms[0])
       }
     }
   }
 
-  useEffect(selectStartRoom, [namespace, user])
+  useEffect(selectStartRoom, [currentRoom, rooms, user])
 
   return <div className="Rooms">
-    <h3>{namespace.title || 'Rooms'}</h3>
+    <h3>{nsTitle || 'Rooms'}</h3>
     <ul className='room-list'>
       {
-        namespace.rooms.map(({ id, ...room }) => <RoomItem key={id}
+        rooms.map(({ id, ...room }) => <RoomItem key={id}
           room={room}
           selected={selected(room)}
           onClick={selectRoom}
@@ -46,8 +45,12 @@ const Rooms = ({ namespace, user, selectRoom }) => {
 }
 
 const mapState = (state) => {
+  const namespace = state.namespace || {}
+
   return {
-    namespace: state.namespace || { rooms: [] },
+    nsTitle: namespace.title,
+    currentRoom: namespace.currentRoom,
+    rooms: namespace.rooms || [],
     user: state.user,
   }
 }
