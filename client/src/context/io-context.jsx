e28010670361c6a4a8266feda8fdb0a9a1ae1b1a
James Abney <james.abney@gmail.com>
2020-01-22 09:09:50 -0800

    Remove file from project

--- client/src/context/io-context.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import io from 'socket.io-client'
-
-export const IoContext = React.createContext(null)
-
-export const IoProvider = ({ children }) => {
-  const socket = io(window.location.href)
-  return <IoContext.Provider value={socket}>{children}</IoContext.Provider>
-}
